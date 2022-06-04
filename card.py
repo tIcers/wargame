379084e63a80894c9bd508b91bcc7b90a3f0c243
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-06-04 17:41:24 +0900

    implemented war condition

--- card.py
+++ card.py
@@ -34,24 +34,125 @@ class Deck:
 
 class Player:
 
-    def __init__(self,name):
+    def __init__(self, name):
         self.name = name
-        self.all_cards =[]
+        self.all_cards = []
 
     def remove_one(self):
-        return self.all_cards.pop(0) # remove the first/ top card
+        return self.all_cards.pop(0)  # remove the first/ top card
 
-    def add_cards(self,new_cards):
-        if type(new_cards) == type([]): # list of multiple cards obj
+    def add_cards(self, new_cards):
+        if type(new_cards) == type([]):  # list of multiple cards obj
             self.all_cards.extend(new_cards)
         else:
-            self.all_cards.append(new_cards) # for single card
+            self.all_cards.append(new_cards)  # for single card
 
     def __str__(self):
         return f"Player {self.name} has {len(self.all_cards)} cards."
 
 
-new_player = Player("Atsuki")
-print(new_player) # this is the test for player class
+# new_player = Player("A")
+# print(new_player) # this is the test for player class
 
 
+# game setup
+
+player1 = Player("1")
+player2 = Player("2")
+
+new_deck = Deck()
+new_deck.shuffle_deck()
+
+# split cards
+
+for x in range(26):  # 52 / 2
+    player1.all_cards(new_deck.deal_one())
+    player2.all_cards(new_deck.deal_one())
+
+game_is_on = True
+
+round_num = 0
+
+while game_is_on:
+    round_num += 1
+    print(f"Round {round_num}")
+
+    # check number of cards player 1 has
+    if len(player1.all_cards) == 0:
+        print(f"Player1, out of cards! Player 2 Wins.")
+        game_is_on = False
+        break
+
+    if len(player2.all_cards) == 0:
+        print(f"Player2, out of cards! Player 1 Wins.")
+        game_is_on = False
+        break
+
+    # start new round from here
+
+    player1_one_cards = []
+    player1_one_cards.append(player1.remove_one())
+
+    player2_two_cards = []
+    player2_two_cards.append(player2.remove_one())
+
+    # loop war condition since it could be happening more than one war in the game
+
+    at_war = True
+
+    # three condition
+    # 1. player one > player two
+    # 2. add cards to player one
+    # 3 at war = False
+
+    # 1. player two > player two
+    # 2. add cards to player two
+    # 3 at war = False
+
+    # 1. player one == player two
+    # 2. check if player has enough card to continue playing
+    # 3  draw additinoal cards
+    # 4 loop to first step
+    while at_war:
+        if player1_one_cards[-1].value > player2_two_cards[-1].value:
+            # Player One gets the cards
+            player1_one_cards.add_cards(player1_one_cards)
+            player1_one_cards.add_cards(player2_two_cards)
+
+            # No Longer at "war" , time for next round
+            at_war = False
+
+            # Player Two Has higher Card
+        elif player1_one_cards[-1].value < player2_two_cards[-1].value:
+
+            # Player Two gets the cards
+            player2_two_cards.add_cards(player1_one_cards)
+            player2_two_cards.add_cards(player2_two_cards)
+
+            # No Longer at "war" , time for next round
+            at_war = False
+
+        else:
+            print('WAR!')
+            # This occurs when the cards are equal.
+            # We'll grab another card each and continue the current war.
+
+            # First check to see if player has enough cards
+
+            # Check to see if a player is out of cards:
+            if len(player1_one_cards.all_cards) < 5:
+                print("Player One unable to play war! Game Over at War")
+                print("Player Two Wins! Player One Loses!")
+                game_on = False
+                break
+
+            elif len(player1_one_cards.all_cards) < 5:
+                print("Player Two unable to play war! Game Over at War")
+                print("Player One Wins! Player One Loses!")
+                game_on = False
+                break
+            # Otherwise, we're still at war, so we'll add the next cards
+            else:
+                for num in range(5):
+                    player1_one_cards.append(player1_one_cards.remove_one())
+                    player2_two_cards.append(player2_two_cards.remove_one())
